7cacfb6da791b5abc4e8728313d4cc6899358d5d
hccdyy <145318934+hccdyy@users.noreply.github.com>
2025-06-26 23:46:41 +0800

    did the base code for the commands
    probs need to revise and fix it up a bit tho

--- Process.cpp
+++ Process.cpp
@@ -2,6 +2,9 @@
 #include "PrintCommand.h"
 #include <iostream>
 
+#include <random>
+#include <ctime>
+
 Process::Process(int pid, const std::string& name, int currentLine, int totalLines, const std::string& timestamp, const std::string& status)
     : pid(pid), name(name), currentLine(currentLine), totalLines(totalLines), timestamp(timestamp), status(status) {
     this->cpuId = -1; // Default CPU core ID, can be set later
@@ -18,12 +21,110 @@ std::time_t Process::getStartTime() const {
     return startTime;
 }
 
+//was edited out 
+/*
 void Process::createPrintCommands(int totalIns) {
     for (int i = 0; i < totalIns; ++i) {
         std::string msg = "Hello world from " + name + "!";
         commands.push_back(new PrintCommand(msg));
     }
+}*/
+
+int getRandomInt(int min, int max) {
+    static std::mt19937 rng(static_cast<unsigned int>(std::time(nullptr)));
+    std::uniform_int_distribution<int> dist(min, max);
+    return dist(rng);
 }
+
+void Process::createPrintCommands(int totalIns) {
+    static const std::vector<std::string> instrTypes = {
+        "PRINT", "DECLARE", "ADD", "SUBTRACT", "SLEEP", "FOR"
+    };
+
+    int i = 0;
+    while (i < totalIns) {
+        std::string type = instrTypes[getRandomInt(0, instrTypes.size() - 1)];
+        std::string msg;
+
+        if (type == "FOR" && i + 3 <= totalIns) {
+            // Always generate 3 consecutive FORs
+            for (int j = 0; j < 3; ++j) {
+                msg = "FOR([PRINT(\"Hello world\")], 3)";
+                commands.push_back(new PrintCommand(msg));
+                ++i;
+            }
+            continue;
+        }
+
+        if (type == "PRINT") {
+            msg = "PRINT(\"Hello world from " + name + "!\")";
+        }
+        else if (type == "DECLARE") {
+            std::string var = "var" + std::to_string(getRandomInt(1, 5));
+            int value = getRandomInt(0, 65535);
+            msg = "DECLARE(" + var + ", " + std::to_string(value) + ")";
+        }
+        else if (type == "ADD" || type == "SUBTRACT") {
+            std::string target = "var" + std::to_string(getRandomInt(1, 5));
+            std::string src1 = (getRandomInt(0, 1) == 0)
+                ? std::to_string(getRandomInt(0, 100))
+                : "var" + std::to_string(getRandomInt(1, 5));
+            std::string src2 = (getRandomInt(0, 1) == 0)
+                ? std::to_string(getRandomInt(0, 100))
+                : "var" + std::to_string(getRandomInt(1, 5));
+            msg = type + "(" + target + ", " + src1 + ", " + src2 + ")";
+        }
+        else if (type == "SLEEP") {
+            int ticks = getRandomInt(1, 50);
+            msg = "SLEEP(" + std::to_string(ticks) + ")";
+        }
+
+        commands.push_back(new PrintCommand(msg));
+        ++i;
+    }
+}
+
+
+/*void Process::createPrintCommands(int totalIns) {
+    static const std::vector<std::string> instrTypes = {
+        "PRINT", "DECLARE", "ADD", "SUBTRACT", "SLEEP", "FOR"
+    };
+
+    for (int i = 0; i < totalIns; ++i) {
+        std::string type = instrTypes[getRandomInt(0, instrTypes.size() - 1)];
+        std::string msg;
+
+        if (type == "PRINT") {
+            msg = "PRINT(\"Hello world from " + name + "!\")";
+        }
+        else if (type == "DECLARE") {
+            std::string var = "var" + std::to_string(getRandomInt(1, 5));
+            int value = getRandomInt(0, 65535);
+            msg = "DECLARE(" + var + ", " + std::to_string(value) + ")";
+        }
+        else if (type == "ADD" || type == "SUBTRACT") {
+            std::string target = "var" + std::to_string(getRandomInt(1, 5));
+            std::string src1 = (getRandomInt(0, 1) == 0) 
+                ? std::to_string(getRandomInt(0, 100)) 
+                : "var" + std::to_string(getRandomInt(1, 5));
+            std::string src2 = (getRandomInt(0, 1) == 0) 
+                ? std::to_string(getRandomInt(0, 100)) 
+                : "var" + std::to_string(getRandomInt(1, 5));
+            msg = type + "(" + target + ", " + src1 + ", " + src2 + ")";
+        }
+        else if (type == "SLEEP") {
+            int ticks = getRandomInt(1, 50);
+            msg = "SLEEP(" + std::to_string(ticks) + ")";
+        }
+        else if (type == "FOR") {
+            int repeats = getRandomInt(2, 4);
+            std::string inner = "PRINT(\"Hello world\")";  // Simplified dummy content
+            msg = "FOR([" + inner + "], " + std::to_string(repeats) + ")";
+        }
+    
+        commands.push_back(new PrintCommand(msg));
+    }
+}*/
 
 void Process::executeCurrentCommand(int cpuId, std::string processName, std::string time) {
     if (currentLine < commands.size()) {
